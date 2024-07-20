8547103c7685113673b18cc55582fa98d3a9e9ea
Maxence Guidez <maxence.guigui@gmail.com>
2024-07-20 07:47:06 +0200

    FPS Controller now using new input system (WIP)
    Somes issues to fix :
    - Upgrade code
    - Look while moving not updating the direction

--- Mimeo/Assets/Scripts/FPSController.cs
+++ Mimeo/Assets/Scripts/FPSController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class FPSController : MonoBehaviour
@@ -6,59 +7,84 @@ public class FPSController : MonoBehaviour
     public Camera playerCamera;
     public float moveSpeed = 6f;
     public float runSpeed = 12f;
-    public float lookSpeed = 2f;
+    public float lookSpeed = 25f;
     public float lookXLimit = 85f;
-    public bool canMove = true;
 
-    CharacterController _characterController;
-    Vector3 _moveDirection = Vector3.zero;
-    float _rotationX = 0;
+    private CharacterController _characterController;
+    private MainInputs _mainInputs;
+    private Vector3 _moveDirection = Vector3.zero;
+    private Vector3 _currentMoveDirection = Vector3.zero;
+    private float _currentSpeed;
+    private float _rotationX;
     
+    private void Awake()
+    {
+        _mainInputs = new MainInputs();
+    }
+
+    private void OnEnable()
+    {
+        _mainInputs.FPSController.Enable();
+        _mainInputs.FPSController.Move.performed += Move;
+        _mainInputs.FPSController.Look.performed += Look;
+        _mainInputs.FPSController.Sprint.performed += Sprint;
+        _mainInputs.FPSController.Sprint.canceled += SprintCanceled;
+    }
+
+    private void OnDisable()
+    {
+        _mainInputs.FPSController.Disable();
+        _mainInputs.FPSController.Move.performed -= Move;
+        _mainInputs.FPSController.Look.performed -= Look;
+        _mainInputs.FPSController.Sprint.performed -= Sprint;
+        _mainInputs.FPSController.Sprint.canceled -= SprintCanceled;
+    }
+
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        _currentSpeed = moveSpeed;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        #region Handles Movement
+        _currentMoveDirection = Vector3.Lerp(_currentMoveDirection, _moveDirection, Time.deltaTime * 10f);
+        _characterController.Move(_currentMoveDirection * (_currentSpeed * Time.deltaTime));
+    }
+
+    private void Move(InputAction.CallbackContext context)
+    {
+        Vector3 inputVector = context.ReadValue<Vector3>();
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
         Vector3 up = transform.TransformDirection(Vector3.up);
+        _moveDirection = (forward * inputVector.z) + (right * inputVector.x) + (up * inputVector.y);
+    }
 
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeed = isRunning ? runSpeed : moveSpeed;
+    private void Look(InputAction.CallbackContext context)
+    {
+        Vector2 inputVector = context.ReadValue<Vector2>();
 
-        float curSpeedX = canMove ? curSpeed * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? curSpeed * Input.GetAxis("Horizontal") : 0;
-        float curSpeedZ = 0;
+        float mouseX = inputVector.x * lookSpeed * Time.deltaTime;
+        float mouseY = inputVector.y * lookSpeed * Time.deltaTime;
 
-        if (canMove)
-        {
-            if (Input.GetKey(KeyCode.E))
-            {
-                curSpeedZ = curSpeed;
-            }
-            else if (Input.GetKey(KeyCode.Q))
-            {
-                curSpeedZ = -curSpeed;
-            }
-        }
+        _rotationX -= mouseY;
+        _rotationX = Mathf.Clamp(_rotationX, -lookXLimit, lookXLimit);
+        
+        playerCamera.transform.localRotation = Quaternion.Euler(_rotationX, 0, 0);
 
-        _moveDirection = (forward * curSpeedX) + (right * curSpeedY) + (up * curSpeedZ);
-        _characterController.Move(_moveDirection * Time.deltaTime);
-        #endregion
+        transform.Rotate(Vector3.up * mouseX);
+    }
 
-        #region Handles Rotation
-        if (canMove)
-        {
-            _rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-            _rotationX = Mathf.Clamp(_rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(_rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
-        }
-        #endregion
+    private void Sprint(InputAction.CallbackContext context)
+    {
+        _currentSpeed = runSpeed;
+    }
+
+    private void SprintCanceled(InputAction.CallbackContext context)
+    {
+        _currentSpeed = moveSpeed;
     }
 }
