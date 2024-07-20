df9570e0b2dd7349d896ac2b4532d88407f1340c
Maxence Guidez <maxence.guigui@gmail.com>
2024-07-20 07:53:44 +0200

    Fix FPSController script

--- Mimeo/Assets/Scripts/FPSController.cs
+++ Mimeo/Assets/Scripts/FPSController.cs
@@ -13,10 +13,9 @@ public class FPSController : MonoBehaviour
     private CharacterController _characterController;
     private MainInputs _mainInputs;
     private Vector3 _moveDirection = Vector3.zero;
-    private Vector3 _currentMoveDirection = Vector3.zero;
     private float _currentSpeed;
     private float _rotationX;
-    
+
     private void Awake()
     {
         _mainInputs = new MainInputs();
@@ -25,8 +24,6 @@ public class FPSController : MonoBehaviour
     private void OnEnable()
     {
         _mainInputs.FPSController.Enable();
-        _mainInputs.FPSController.Move.performed += Move;
-        _mainInputs.FPSController.Look.performed += Look;
         _mainInputs.FPSController.Sprint.performed += Sprint;
         _mainInputs.FPSController.Sprint.canceled += SprintCanceled;
     }
@@ -34,8 +31,6 @@ public class FPSController : MonoBehaviour
     private void OnDisable()
     {
         _mainInputs.FPSController.Disable();
-        _mainInputs.FPSController.Move.performed -= Move;
-        _mainInputs.FPSController.Look.performed -= Look;
         _mainInputs.FPSController.Sprint.performed -= Sprint;
         _mainInputs.FPSController.Sprint.canceled -= SprintCanceled;
     }
@@ -48,33 +43,34 @@ public class FPSController : MonoBehaviour
         _currentSpeed = moveSpeed;
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        _currentMoveDirection = Vector3.Lerp(_currentMoveDirection, _moveDirection, Time.deltaTime * 10f);
-        _characterController.Move(_currentMoveDirection * (_currentSpeed * Time.deltaTime));
+        HandleMovement();
+        HandleLook();
     }
 
-    private void Move(InputAction.CallbackContext context)
+    private void HandleMovement()
     {
-        Vector3 inputVector = context.ReadValue<Vector3>();
+        Vector3 inputVector = _mainInputs.FPSController.Move.ReadValue<Vector3>();
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
         Vector3 up = transform.TransformDirection(Vector3.up);
-        _moveDirection = (forward * inputVector.z) + (right * inputVector.x) + (up * inputVector.y);
+
+        _moveDirection = (forward * inputVector.z + right * inputVector.x + up * inputVector.y).normalized;
+        _characterController.Move(_moveDirection * (_currentSpeed * Time.deltaTime));
     }
 
-    private void Look(InputAction.CallbackContext context)
+    private void HandleLook()
     {
-        Vector2 inputVector = context.ReadValue<Vector2>();
+        Vector2 inputVector = _mainInputs.FPSController.Look.ReadValue<Vector2>();
 
         float mouseX = inputVector.x * lookSpeed * Time.deltaTime;
         float mouseY = inputVector.y * lookSpeed * Time.deltaTime;
 
         _rotationX -= mouseY;
         _rotationX = Mathf.Clamp(_rotationX, -lookXLimit, lookXLimit);
-        
-        playerCamera.transform.localRotation = Quaternion.Euler(_rotationX, 0, 0);
 
+        playerCamera.transform.localRotation = Quaternion.Euler(_rotationX, 0, 0);
         transform.Rotate(Vector3.up * mouseX);
     }
 
