cedf5cfaf319469898e269dd642ec8d930d3d752
Maxence Guidez <maxence.guigui@gmail.com>
2024-07-21 21:08:38 +0200

    Can change color and texture of selectable elements (WIP)
    Issues to fix :
    - Upgrade dropdowns
    - Save new material
    - Original color and texture
    - Change selection and highlight materials

--- Mimeo/Assets/Scripts/Menus/PanelInfos.cs
+++ Mimeo/Assets/Scripts/Menus/PanelInfos.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -10,19 +11,27 @@ public class PanelInfos : MonoBehaviour
     public TMP_Dropdown dropdownColor;
     public TMP_Dropdown dropdownTexture;
     public float animationDuration = .2f;
+    public Material[] colorMaterials;
+    public Material[] textureMaterials;
 
     private SelectableElement _selectedElement;
     private Vector3 _closedPosition;
     private Vector3 _openPosition;
     private bool _isAnimating;
+    private Material _combinedMaterial;
     
     void Start()
     {
         _closedPosition = transform.localPosition;
         _openPosition = _closedPosition - new Vector3(0, 650, 0);
+        _combinedMaterial = new Material(Shader.Find("Standard"));
+        
+        PopulateDropdowns();
 
         textFieldName.onValueChanged.AddListener(delegate {OnNameChange(); });
         textFieldDescription.onValueChanged.AddListener(delegate {OnDescriptionChange(); });
+        dropdownColor.onValueChanged.AddListener(delegate { UpdateMaterial(); });
+        dropdownTexture.onValueChanged.AddListener(delegate { UpdateMaterial(); });
     }
 
     public void Open(SelectableElement selected)
@@ -80,5 +89,48 @@ public class PanelInfos : MonoBehaviour
     void OnDescriptionChange()
     {
         _selectedElement.description = textFieldDescription.text;
+    }
+    
+    void PopulateDropdowns()
+    {
+        dropdownColor.ClearOptions();
+        dropdownTexture.ClearOptions();
+
+        List<string> colorOptions = new List<string>();
+        colorOptions.Add("Original color");
+        foreach (Material mat in colorMaterials)
+        {
+            colorOptions.Add(mat.name);
+        }
+        dropdownColor.AddOptions(colorOptions);
+        dropdownColor.value = 0;
+
+        List<string> textureOptions = new List<string>();
+        textureOptions.Add("Original texture");
+        foreach (Material mat in textureMaterials)
+        {
+            textureOptions.Add(mat.name);
+        }
+        dropdownTexture.AddOptions(textureOptions);
+        dropdownTexture.value = 0;
+    }
+    
+    void UpdateMaterial()
+    {
+        if (dropdownColor.value == 0) return;
+        if (dropdownTexture.value == 0) return;
+        
+        if (dropdownColor.value == 0 && dropdownTexture.value == 0)
+        {
+            _selectedElement.ResetMaterial();
+        }
+        
+        Material selectedColorMaterial = colorMaterials[dropdownColor.value - 1];
+        Material selectedTextureMaterial = textureMaterials[dropdownTexture.value - 1];
+
+        _combinedMaterial.color = selectedColorMaterial.color;
+        _combinedMaterial.mainTexture = selectedTextureMaterial.mainTexture;
+
+        _selectedElement.SetMaterial(_combinedMaterial);
     }
 }
--- Mimeo/Assets/Scripts/SelectableElement.cs
+++ Mimeo/Assets/Scripts/SelectableElement.cs
@@ -1,8 +1,31 @@
+using System;
 using UnityEngine;
 
 public class SelectableElement : MonoBehaviour
 {
     public string description = "NO DESCRIPTION";
-    public Color color;
-    public Texture texture;
+
+    private MeshRenderer _renderer;
+    private Material[] _originalMaterials;
+
+    private void Start()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+        _originalMaterials = _renderer.materials;
+    }
+
+    public void SetMaterial(Material newMaterial)
+    {
+        Material[] newMaterials = new Material[_renderer.materials.Length];
+        for (int i = 0; i < newMaterials.Length; i++)
+        {
+            newMaterials[i] = newMaterial;
+        }
+        _renderer.materials = newMaterials;
+    }
+
+    public void ResetMaterial()
+    {
+        _renderer.materials = _originalMaterials;
+    }
 }
