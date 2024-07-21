07803598dd2ffcadb682fa6954064a9c5a65e4c5
Maxence Guidez <maxence.guigui@gmail.com>
2024-07-21 09:08:16 +0200

    Fix issue settings not opening in main menu

--- Mimeo/Assets/Scripts/Menus/MainMenu.cs
+++ Mimeo/Assets/Scripts/Menus/MainMenu.cs
@@ -5,22 +5,17 @@ public class MainMenu : MonoBehaviour
 {
     public SettingsMenu settingsMenu;
 
-    private void Start()
-    {
-        settingsMenu.gameObject.SetActive(false);
-    }
-
     public void OnClickBtnVisit()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    
+
     public void OnClickBtnSettings()
     {
         gameObject.SetActive(false);
         settingsMenu.gameObject.SetActive(true);
     }
-    
+
     public void OnClickBtnQuit()
     {
         Application.Quit();
