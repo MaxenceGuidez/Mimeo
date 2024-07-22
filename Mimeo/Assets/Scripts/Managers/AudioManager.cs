19b62d9c66c51a0f8ab7a6178dffa7acdd6e5ff1
Maxence Guidez <maxence.guigui@gmail.com>
2024-07-22 04:58:11 +0200

    Add music & Add mixers

---
+++ Mimeo/Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] playlist;
+    public AudioSource audioSource;
+    public AudioMixerGroup sfxMixer;
+    
+    private int _musicIndex = 0;
+    
+    public static AudioManager instance { get; private set; }
+    
+    private void Awake()
+    {
+        if (instance && instance != this) Destroy(this); 
+        else instance = this;
+        
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        audioSource.clip = playlist[0];
+        audioSource.Play();
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying) PlayNextSong();
+    }
+
+    void PlayNextSong()
+    {
+        _musicIndex = (_musicIndex + 1) % playlist.Length;
+        audioSource.clip = playlist[_musicIndex];
+        audioSource.Play();
+    }
+
+    public AudioSource PlayClipAt(AudioClip clip, Vector3 pos)
+    {
+        GameObject tempGo = new GameObject("TempAudio");
+        tempGo.transform.position = pos;
+        AudioSource audioSource = tempGo.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.outputAudioMixerGroup = sfxMixer;
+        audioSource.Play();
+        Destroy(tempGo, clip.length);
+        return audioSource;
+    }
+}
---
+++ Mimeo/Assets/Scripts/Managers/InputsManager.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputsManager : MonoBehaviour
+{
+    public PauseMenu pauseMenu;
+    public MainInputs mainInputs;
+    
+    public static InputsManager instance { get; private set; }
+    
+    private void Awake()
+    {
+        if (instance && instance != this) Destroy(this); 
+        else instance = this;
+        
+        mainInputs = new MainInputs();
+    }
+    
+    private void OnEnable()
+    {
+        mainInputs.FPSController.Enable();
+        mainInputs.Selector.Enable();
+        mainInputs.Menu.Enable();
+        
+        mainInputs.FPSController.Sprint.performed += Sprint;
+        mainInputs.FPSController.Sprint.canceled += SprintCanceled;
+        mainInputs.Selector.Select.performed += Select;
+        mainInputs.Selector.Unselect.performed += Unselect;
+        mainInputs.Menu.Pause.performed += Pause;
+        mainInputs.Menu.Settings.performed += OpenSettings;
+    }
+
+    private void OnDisable()
+    {
+        mainInputs.FPSController.Disable();
+        mainInputs.Selector.Disable();
+        mainInputs.Menu.Disable();
+        
+        mainInputs.FPSController.Sprint.performed -= Sprint;
+        mainInputs.FPSController.Sprint.canceled -= SprintCanceled;
+        mainInputs.Selector.Select.performed -= Select;
+        mainInputs.Selector.Unselect.performed -= Unselect;
+        mainInputs.Menu.Pause.performed -= Pause;
+        mainInputs.Menu.Settings.performed -= OpenSettings;
+    }
+
+    private void Sprint(InputAction.CallbackContext context)
+    {
+        FPSController.instance.Sprint(true);
+    }
+
+    private void SprintCanceled(InputAction.CallbackContext context)
+    {
+        FPSController.instance.Sprint(false);
+    }
+
+    private void Select(InputAction.CallbackContext context)
+    {
+        Selector.instance.Select();
+    }
+
+    private void Unselect(InputAction.CallbackContext context)
+    {
+        Selector.instance.Unselect();
+    }
+
+    private void Pause(InputAction.CallbackContext context)
+    {
+        pauseMenu.Pause();
+    }
+
+    private void OpenSettings(InputAction.CallbackContext context)
+    {
+        pauseMenu.Pause(true);
+    }
+}
